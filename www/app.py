4689b7da21441381443541c1f9cd8c7a2079f029
hellokingyip <hellokingyip@gmail.com>
2016-01-05 15:22:00 +0800

    day2

---
+++ www/app.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+import json
+import time
+from datetime import datetime
+from aiohttp import web
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+def index(request):
+    return web.Response(body=b'<h1>Web app in python.</h1>')
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info("server start at 127.0.0.1:9000...")
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
